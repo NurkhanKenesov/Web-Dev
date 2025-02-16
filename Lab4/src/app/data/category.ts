61e245ac81219b194e50c6aabd2856695500dc9c
NurkhanKenesov <nurkhan200505@gmail.com>
2025-02-16 18:04:54 +0500

    Lab4

---
+++ Lab4/src/app/data/category.ts
@@ -0,0 +1,6 @@
+export const categories = [
+  {type: "Motocycle", choose : false},
+  {type: "Phone", choose : false},
+  {type: "Laptop", choose : false},
+  {type: "Headphone", choose : false}
+]
---
+++ Lab4/src/app/data/products.ts
@@ -0,0 +1,64 @@
+import { Products } from "../models/product";
+
+export const products: Products[] = [
+  {
+    "id": 1,
+    "category": "Motocycle",
+    "title": "Мотоцикл Arlan ENDURO FMZ 01 черный",
+    "price": 670000,
+    "liked": false,
+    "visible": true,
+    "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb6/he8/50196375633950/arlan-enduro-fmz-01-102283789-1.jpg",
+    "link": "https://kaspi.kz/shop/p/arlan-enduro-fmz-01-chernyi-102283789",
+    "characteristics": "Тип мотоцикла: кроссовый<br>Максимальная скорость: 120 км/ч<br>Цвет: черный<br>Тип двигателя: 4-тактный бензиновый<br>Объем двигателя: 250 куб. см<br>Запуск двигателя: электростартер/кикстартер<br>Трансмиссия: механическая",
+    "rating": { "rate": 3.9, "count": 120 }
+  },
+  {
+    "id": 2,
+    "category": "Phone",
+    "title": "Смартфон Apple iPhone 14 Pro Max 256Gb фиолетовый",
+    "price": 672600,
+    "liked": false,
+    "visible": true,
+    "image": "https://resources.cdn-kaspi.kz/img/m/p/h19/h85/64508108931102.jpg?format=gallery-medium",
+    "link": "https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342",
+    "characteristics": "Технология NFC: Да<br>Цвет: фиолетовый<br>Тип экрана: OLED, Super Retina XDR<br>Диагональ: 6.7 дюйм<br>ОЗУ: 6 ГБ<br>Процессор: Apple A16 Bionic<br>Встроенная память: 256 ГБ<br>Аккумулятор: 3095 мАч",
+    "rating": { "rate": 5, "count": 120 }
+  },
+  {
+    "id": 3,
+    "category": "Laptop",
+    "title": "Ноутбук Apple MacBook Air 13 MGN63 серый",
+    "price": 477293,
+    "liked": false,
+    "visible": true,
+    "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h65/h0f/33125684084766/apple-macbook-air-2020-13-3-mgn63-seryj-100797845-1.jpg",
+    "link": "https://kaspi.kz/shop/p/apple-macbook-air-13-mgn63-seryi-100797845",
+    "characteristics": "Диагональ экрана: 13.3 дюйма<br>Процессор: Apple M1<br>Видеокарта: Apple M1 GPU<br>ОЗУ: 8 ГБ<br>Тип накопителя: SSD<br>Объем накопителя: 256 ГБ",
+    "rating": { "rate": 5, "count": 120 }
+  },
+  {
+    "id": 4,
+    "category": "Laptop",
+    "title": "Ноутбук Lenovo IdeaPad 3 15ITL6 серебристый",
+    "price": 169990,
+    "liked": false,
+    "visible": true,
+    "image": "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8f/h3a/66993674190878/lenovo-ideapad-3-15itl6-82h8005grk-serebristyi-108090705-1.jpg",
+    "link": "https://kaspi.kz/shop/p/lenovo-ideapad-3-15itl6-82h8005grk-serebristyi-108090705",
+    "characteristics": "Диагональ экрана: 15.6 дюйма<br>Процессор: Intel Pentium Gold 7505<br>Видеокарта: Intel UHD Graphics<br>ОЗУ: 8 ГБ<br>Тип накопителя: SSD<br>Объем накопителя: 256 ГБ",
+    "rating": { "rate": 5, "count": 120 }
+  },
+  {
+    "id": 5,
+    "category": "Phone",
+    "title": "Смартфон Xiaomi Redmi Note 10 Pro 8 ГБ/256 ГБ серый",
+    "price": 128350,
+    "liked": false,
+    "visible": true,
+    "image": "https://resources.cdn-kaspi.kz/img/m/p/h94/h74/64487156645918.jpg?format=gallery-medium",
+    "link": "https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005",
+    "characteristics": "Технология NFC: Да<br>Цвет: серый<br>Тип экрана: AMOLED, 120 Гц<br>Диагональ: 6.67 дюйма<br>ОЗУ: 8 ГБ<br>Процессор: Qualcomm Snapdragon 732G<br>Встроенная память: 256 ГБ<br>Аккумулятор: 5020 мАч",
+    "rating": { "rate": 4.8, "count": 120 }
+  }
+];
